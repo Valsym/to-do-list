612fdfb9955be4670881f11eb843baed8e704187
Valsym <fanski@yandex.ru>
2023-03-20 23:57:41 +0300

    Перенес все функции в helpers.php

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-function listItemCount(array $tasks, $project) {
-    $sum = 0;
-    foreach($tasks as $task) {
-        if ($task['category'] === $project)
-            $sum++;
-    }
-    return $sum;
-}
